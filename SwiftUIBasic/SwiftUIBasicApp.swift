44a89c2f06b7543e8ed07c23d6da96c7e2fd1f55
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-09-30 22:10:38 +0300

    Initial Commit

---
+++ SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by Zeki Furkan Durum on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
